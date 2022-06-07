5a865feaa6195f8c2587c767b315e704eb36634c
Clement Legrand <clement@whentocop.fr>
2022-06-07 15:33:20 +0200

    try fix payment error

--- data/products.js
+++ data/products.js
@@ -2,28 +2,28 @@ export const products = [
   {
     id: "1",
     name: "Cap",
-    price: 35.00,
+    price: 35,
     description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards.",
     url: "/api/products/1"
   },
   {
     id: "2",
     name: "Hoodie",
-    price: 85.00,
+    price: 85,
     description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards.",
     url: "/api/products/2"
   },
   {
     id: "3",
     name: "Tee",
-    price: 40.00,
+    price: 40,
     description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards.",
     url: "/api/products/3"
   },
   {
     id: "4",
     name: "Tee",
-    price: 30.00,
+    price: 30,
     description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards.",
     url: "/api/products/4"
   }
